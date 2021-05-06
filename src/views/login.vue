13ac91e5224fc4ce2a5899073b7480476b4308fe
皓粼 <hujian.hu@antgroup.com>
2021-05-06 23:54:21 +0800

    去掉验证码校验

--- src/views/login.vue
+++ src/views/login.vue
@@ -18,20 +18,20 @@
           <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
-      <el-form-item prop="code">
-        <el-input
-          v-model="loginForm.code"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" />
-        </div>
-      </el-form-item>
+<!--      <el-form-item prop="code">-->
+<!--        <el-input-->
+<!--          v-model="loginForm.code"-->
+<!--          auto-complete="off"-->
+<!--          placeholder="验证码"-->
+<!--          style="width: 63%"-->
+<!--          @keyup.enter.native="handleLogin"-->
+<!--        >-->
+<!--          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />-->
+<!--        </el-input>-->
+<!--        <div class="login-code">-->
+<!--          <img :src="codeUrl" @click="getCode" />-->
+<!--        </div>-->
+<!--      </el-form-item>-->
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button
@@ -93,7 +93,7 @@ export default {
     }
   },
   created() {
-    this.getCode();
+    // this.getCode();
     this.getCookie();
   },
   methods: {
@@ -133,7 +133,7 @@ export default {
             })
             .catch(() => {
               this.loading = false;
-              this.getCode();
+              // this.getCode();
             });
         }
       });
